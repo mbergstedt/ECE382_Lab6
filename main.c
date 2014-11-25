9fea6a05b62e2dac141e3632d3e3a2dd9d6d8d24
Matt Bergstedt <C16matthew.bergstedt@usafa.edu>
2014-11-24 21:00:54 -0700

    had initial setup for msp pins

--- main.c
+++ main.c
@@ -9,14 +9,29 @@
 void main(void) {
     WDTCTL = WDTPW|WDTHOLD;                 // stop the watchdog timer
 
-    P1DIR &= ~BIT3;
-    P1REN |= BIT3;
+    // use P2.0 as the enable for left motor, start with it off
+    P2DIR |= BIT0;
+    P2OUT &= ~BIT0;
 
+    // use P2.1 as direction for the left motor, 0 goes cw, 1 goes ccw
+    P2DIR |= BIT1;
+    P2OUT &= ~BIT1;
+
+    // P2.2 as the pwm for the left motor
     P2DIR |= BIT2;							// P2.2 is associated with TA1CCR1
     P2SEL |= BIT2;							// P2.2 is associated with TA1CCTL1
 
+    // use P2.3 as the enable for right motor, start with it off
+    P2DIR |= BIT3;
+    P2OUT &= ~BIT3;
+
+    // P2.4 as the pwm for the right motor
     P2DIR |= BIT4;							// P2.4 is associated with TA1CCR2
     P2SEL |= BIT4;							// P2.4 is associated with TA1CCTL2
+
+    // use P2.5 as direction for the left motor, 0 goes cw, 1 goes ccw
+    P2DIR |= BIT5;
+    P2OUT &= ~BIT5;
 
 	TA1CTL = ID_3 | TASSEL_2 | MC_1;		// Use 1:8 presclar off MCLK
     TA1CCR0 = 0x0100;						// set signal period
